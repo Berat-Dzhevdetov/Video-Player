6bc054e8c2e8602c5fe9eda3ae2ac6ee2eada35a
Berat Yuseinov <Berat@students.softuni.bg>
2022-05-14 15:12:15 +0300

    Implemented and designed theater, mini and full screen mode

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const video = document.querySelector('video');
 const videoContainer = document.querySelector('.video-container')
 const playPauseBtn = document.querySelector('.play-pause-btn');
+const theaterBtn = document.querySelector('.theater-player-btn');
+const miniPlayerBtn = document.querySelector('.mini-player-btn');
+const fullScreenBtn = document.querySelector('.full-screen-player-btn');
 
 // Adding event listeners
 // Play/Pause
@@ -9,6 +12,13 @@ playPauseBtn.addEventListener('click', togglePlay);
 video.addEventListener("play", onVideoPlay);
 video.addEventListener("pause", onVideoPause);
 video.addEventListener("click", togglePlay)
+// View Modes
+theaterBtn.addEventListener('click', toggleTheaterMode);
+miniPlayerBtn.addEventListener('click', toggleMiniPlayerMode);
+fullScreenBtn.addEventListener('click', toggleFullScreenMode);
+document.addEventListener('fullscreenchange', toggleDocumentFullScreenMode);
+document.addEventListener('enterpictureinpicture', addVideoPictureInPictureMode);
+document.addEventListener('leavepictureinpicture', leaveVideoPictureInPictureMode);
 
 document.addEventListener("keydown", e => handleUserKeyboardInteraction(e));
 
@@ -27,10 +37,58 @@ function onVideoPause() {
 }
 
 function handleUserKeyboardInteraction(e) {
+    const tagName = document.activeElement.tagName.toLocaleLowerCase();
+
+    if(tagName === "input") return;
+
     switch (e.key.toLowerCase()) {
-        case "k":
         case " ":
+            if(tagName === "button") return;
+        case "k":
             togglePlay();
             break;
+        case "f":
+            toggleFullScreenMode();
+            break;
+        case "t":
+            toggleTheaterMode();
+            break;
+        case "i":
+            toggleMiniPlayerMode();
+            break;
     }
+}
+
+// View Modes
+function toggleTheaterMode() {
+    videoContainer.classList.toggle("theater");
+}
+
+function toggleFullScreenMode() {
+    if (document.fullscreenElement == null) {
+        videoContainer.requestFullscreen();
+    } else {
+        document.exitFullscreen()
+
+    }
+}
+
+function toggleMiniPlayerMode() {
+    if (videoContainer.classList.contains("mini-player")) {
+        document.exitPictureInPicture();
+    } else {
+        video.requestPictureInPicture();
+    }
+}
+
+function toggleDocumentFullScreenMode() {
+    videoContainer.classList.toggle("full-screen", document.fullscreenElement);
+}
+
+function addVideoPictureInPictureMode() {
+    videoContainer.classList.add("mini-player");
+}
+
+function leaveVideoPictureInPictureMode() {
+    videoContainer.classList.remove("mini-player");
 }
